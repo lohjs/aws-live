e08b9d5a6098e1f3f81b32b662fb8656a65164eb
lohjs <104544583+lohjs@users.noreply.github.com>
2024-12-29 21:22:56 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cf7hfepuoqbt.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lohjiashou-employee"
+customregion = "us-east-1"
+
